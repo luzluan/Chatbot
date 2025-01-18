e80a08363a2b81c0106c51cdb0985cd70a36820b
Luan Luz <luandonato@protonmail.com>
2025-01-17 22:48:14 -0300

    checkpoint

--- chatbot.py
+++ chatbot.py
@@ -42,47 +42,8 @@ while condicao_data_nascimento:
         if confirmar(user_input):
             condicao_data_nascimento = False
     else:
-        print("Vamos tentar novamente. \nExemplo: 01/01/2000\n")
+        print("Vamos tentar novamente. \nExemplo: 01/01/2000, 01.01.2000 ou 01-01-2000\n")
 
-print("teste")
+print("Perfeito! Vamos prosseguir com seu atendimento.\n")
 
-
-# if re.fullmatch(r"^[a-zA-Z]+$", nome):
-#     print("Tente inserir seu nome novamente.")
-#     nome = input("Qual é o seu nome?\n")
-# idade = input("Agora, qual é a sua idade?\n")
-# verificar = print("Seu nome é", nome, "e a sua idade é", idade)
-
-
-# def dados():
-
-#     nome= input("Olá, diga seu nome.\n")
-#     print(validar_letras(nome))
-#     idade = int(input("Agora, a sua idade.\n"))
-#     print("Então, seu nome é", nome, "e sua idade é", idade)
-
-# def validar_letras(entrada):
-#     if re.fullmatch(r"^[a-zA-Z]+$", entrada):
-#         return True
-#     return False
-
-
-
-# if validar_letras(entrada):
-#     print("Entrada válida!")
-# else:
-#     print("Entrada inválida. Apenas letras são permitidas.")
-
-
-
-# answer = input("\nCorreto?\n s ou n\n")
-# if answer == "s":   
-#     print("Perfeito. Vamos prosseguir!")
-# elif answer == "n":
-#     dados()
-# else:
-#     print("Tente responder com S ou N.")
-
-# #verificação se tem registro dos dados no banco (função)
-
-# print("Vimos que você já está registrado. No que podemos ajudar?")
+opcao = input(...)
